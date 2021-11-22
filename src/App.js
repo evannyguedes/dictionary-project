09ac02112ab38996739b2d8d564adf86af3cd7bc
evannyguedes <evannyguedes@gmail.com>
2021-11-22 21:40:01 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './CSS/App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Word Universe
+      </header>
+    </div>
+  );
+}
+
+
