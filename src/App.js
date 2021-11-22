a5f64dd41624494b86ab710bb16732fdaa16a10e
evannyguedes <evannyguedes@gmail.com>
2021-11-22 22:41:14 +0100

    h3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>Welcome to the</h3>
        <h1>Word Universe</h1>
        <a href="#" className="btn btn-primary">
          Let's go!
