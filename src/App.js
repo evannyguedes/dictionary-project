da21a5275add0f7c18ce677c2c1633b7f8905b4c
evannyguedes <evannyguedes@gmail.com>
2021-11-25 21:34:47 +0100

    take off "Link" from App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 //Pages
 import MainPage from "./MainPage";
 import NotFound from "./NotFound";
