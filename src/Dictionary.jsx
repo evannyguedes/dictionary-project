1c4574e3a9781ec99d9f8d721b869045dfd703f0
evannyguedes <evannyguedes@gmail.com>
2021-11-25 21:10:53 +0100

    Add api documentation url

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -10,9 +10,9 @@ export default function Dictionary(){
     }
 
     function search(event){
-    event.preventDefault();
-    alert(`Searching for ${keyword} definition`)   
-
+    event.preventDefault();  
+    
+    //documentation: https:dictionaryapi.dev/
     let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
     }
