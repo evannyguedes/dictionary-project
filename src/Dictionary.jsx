db795def5b46727a18bd15e39c03d2887a18d28a
evannyguedes <evannyguedes@gmail.com>
2021-12-06 18:02:08 +0100

    Add photos

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -4,27 +4,44 @@ import axios from "axios";
 import Results from "./Results";
 import Rocket from "./media/rocket.png";
 import {Link} from "react-router-dom";
+import Photos from "./Photos";
 
 export default function Dictionary(){
-    const [keyword, setKeyword] = useState("");
+    const [keyword, setKeyword] = useState("earth");
     const [results, setResults] = useState(null);
+    const [loaded, setLoaded] = useState(false);
+    const [photos, setPhotos] = useState(null);
 
-    function handleResponse(response){
+    function handleDictionaryResponse(response){
     setResults(response.data[0]);  
     }
 
-    function search(event){
-    event.preventDefault();  
+    function handlePexelsResponse(response){
+        setPhotos(response.data.photos);  
+        }
     
+    function search(){
     //documentation: https:dictionaryapi.dev/
     let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const apiKeyPexels="563492ad6f91700001000001a056dc5254ea491f9997b38e28da26bd"
+    let apiUrlPexels = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+    const headers = {"Authorization": `Bearer ${apiKeyPexels}`};
+    axios.get(apiUrlPexels, {headers: headers}).then(handlePexelsResponse);
+
     }
 
     function handleKeywordChange(event){
     setKeyword(event.target.value)
     }
 
+    function load(){
+        setLoaded(true);
+        search();
+    }
+
+    if (loaded){
     return (
         <body>
        <div className="Dictionary">
@@ -40,11 +57,15 @@ export default function Dictionary(){
                 </div>
             </div>
             <Results results={results} />
+            <Photos photos={photos} />
             <footer className="Evanny" style={{color:"black;"}}>
            Coded by Évanny Guedes Belarmino
        </footer>
        </div>
        </body>
-       
-   )
+    );
+    } else {
+        load();
+        return "Loading";
+    }
 }
---
+++ src/Photos.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./css/Photos.scss";
+
+export default function Photos(props){
+    if(props.photos){
+        return (
+            <section className="Photos">
+                <div className="row">
+                {props.photos.map(function(photo, index){
+                    return(
+                        <div className="col-2" key={index}>
+                            <a href={photo.src.original} target="_blank" rel="noreferrer">
+                        <img src={photo.src.landscape} alt="photos" className="img-fluid" />
+                        </a>
+                        </div>
+                    )
+                })}
+                </div>
+            </section>
+        )
+    } else {
+        return null;
+    }
+    
+}
